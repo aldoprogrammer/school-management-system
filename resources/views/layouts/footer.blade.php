f9e6d13faaaf76492e6f2ce687102f1f83ec0566
aldoprogrammer <aldobesma@gmail.com>
2024-02-13 17:30:22 +0700

    update

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024, Developed by <a href="https://github.com/aldoprogrammer">Aldo Lata Soba</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+   <!-- Control Sidebar -->
+   <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 });
+
+Route::get('/admin/admin/list', function () {
+    return view('admin.admin.list');
+});
