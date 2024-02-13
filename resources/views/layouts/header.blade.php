d77c2ed683df51b283956f734a3f243cc55540be
aldoprogrammer <aldobesma@gmail.com>
2024-02-13 23:50:26 +0700

    update

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -151,7 +151,7 @@
           <img src="{{url('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -162,6 +162,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               @if (Auth::user()->user_type == 1)
                <li class="nav-item {{ Request::is('admin/dashboard') ? 'menu-open' : '' }}">
                 <a href="{{ url('admin/dashboard')}}" class="nav-link {{ Request::is('admin/dashboard') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -175,6 +176,50 @@
                     <i class="nav-icon fas fa-th"></i>
                     <p>
                         Admin
+                    </p>
+                </a>
+            </li>
+
+
+               @elseif(Auth::user()->user_type == 2)
+               <li class="nav-item {{ Request::is('/teacher/dashboard') ? 'menu-open' : '' }}">
+                <a href="{{ url('teacher/dashboard')}}" class="nav-link {{ Request::is('admin/dashboard') ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                </a>
+            </li>
+
+
+               @elseif(Auth::user()->user_type == 3)
+                <li class="nav-item {{ Request::is('/student/dashboard') ? 'menu-open' : '' }}">
+                    <a href="{{ url('student/dashboard')}}" class="nav-link {{ Request::is('admin/dashboard') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+               @elseif(Auth::user()->user_type == 4)
+                <li class="nav-item {{ Request::is('/parent/dashboard') ? 'menu-open' : '' }}">
+                    <a href="{{ url('parent/dashboard')}}" class="nav-link {{ Request::is('admin/dashboard') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
+
+
+               @endif
+
+
+            <li class="nav-item">
+                <a href="{{ url('logout')}}" class="nav-link">
+                    <i class="nav-icon fas fa-th"></i>
+                    <p>
+                        Logout
                     </p>
                 </a>
             </li>
